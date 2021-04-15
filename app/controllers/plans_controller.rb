21849b007a03eb1265f04b048f2ff0ba28b33547
uki-88 <uki88.nn.1227@gmail.com>
2021-04-16 02:29:58 +0900

    予定作成機能の実装

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -2,4 +2,18 @@ class PlansController < ApplicationController
   def new
     @plan = Plan.new
   end
+  def create
+    @plan = Plan.new(plan_params)
+    if @plan.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def plan_params
+    params.require(:plan).permit(:title, user_ids: [])
+  end
 end
--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,4 +1,6 @@
 class Plan < ApplicationRecord
   has_many :plan_users
   has_many :users, through: :plan_users
+
+  validates :title, presence: true
 end
