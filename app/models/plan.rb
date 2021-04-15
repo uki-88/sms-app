cfef10447eb9b355140a7afc78f47f940d933d9a
uki-88 <uki88.nn.1227@gmail.com>
2021-04-16 01:45:59 +0900

    plan_usersテーブルの作成

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,2 +1,4 @@
 class Plan < ApplicationRecord
+  has_many :plan_users
+  has_many :users, through: :plan_users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_15_161817) do
+ActiveRecord::Schema.define(version: 2021_04_15_163927) do
+
+  create_table "plan_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "plan_id"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["plan_id"], name: "index_plan_users_on_plan_id"
+    t.index ["user_id"], name: "index_plan_users_on_user_id"
+  end
 
   create_table "plans", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
@@ -32,4 +41,6 @@ ActiveRecord::Schema.define(version: 2021_04_15_161817) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "plan_users", "plans"
+  add_foreign_key "plan_users", "users"
 end
