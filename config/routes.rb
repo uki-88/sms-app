c38f120cbdd9b26842ea21d728a2c8baf0c39fb9
uki-88 <uki88.nn.1227@gmail.com>
2021-04-23 03:26:23 +0900

    カレンダーを含めたビューの調整

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "plans#index"
   resources :users, only: [:edit, :update]
-  resources :plans, only: [:new, :create, :show, :destroy] do
+  resources :plans, only: [:index, :new, :create, :show, :destroy] do
     resources :contents, only: [:index, :create]
   end
 end
